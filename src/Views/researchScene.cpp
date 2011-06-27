aa4746656727b6e1300da22deb89d89275eecd71
Miguel Bermudez <miguel@potiondesign.com>
2011-06-27 18:10:00 -0400

    added 3d model viewer functionality

--- src/Views/researchScene.cpp
+++ src/Views/researchScene.cpp
@@ -8,6 +8,12 @@
 
 #include "researchScene.h"
 
+GLfloat lightOnePosition[] = {80.0, -1540, 100.0, 0.0};
+GLfloat lightOneColor[] = {0.82, 0.88, 0.99, 1.0};
+
+GLfloat lightTwoPosition[] = {-80.0, 1540, 100.0, 0.0};
+GLfloat lightTwoColor[] = {0.99, 0.74, 0.32, 1.0};
+
 //------------------------------------------------------------------
 void researchScene::setup() {
     //set up for main scene
@@ -58,6 +64,11 @@ void researchScene::setup() {
     start.setPos(back.rect.width/2,back.rect.height);
     start.setLabel("Start Activity!", &mnhAssets->whitneySemiBold22);
     
+    firstDoubleTap = false;
+    isTouchDown = false;
+    touchX = 0;
+    touchY = 0;
+    
 
 }
 
@@ -80,7 +91,8 @@ void researchScene::update() {
 //        }
 //    }
     
-     
+    if (!isTouchDown)
+        boneModel.setRotation(1, 270 + ofGetElapsedTimef() * 30, 0, 1, 0); 
 }
 
 //------------------------------------------------------------------
@@ -139,7 +151,10 @@ void researchScene::draw() {
             sceneName = "Third Sub Scene!";
             break;
         case MNH_RESEARCH_SCENE_FIFTH:
-            threeD.draw(0,0);
+            init3DViewer(t);
+            drawModel();
+            //threeD.draw(0,0);
+            
             back.draw(0,ofGetHeight()-back.rect.height-50);
             start.draw(ofGetWidth()-start.rect.width,ofGetHeight()-start.rect.height-50);
             sceneName = "Third Sub Scene!";
@@ -148,14 +163,81 @@ void researchScene::draw() {
     ofDisableAlphaBlending();
 }
 
+//--------------------------------------------------------------
+void researchScene::init3DViewer(traumaType trauma){
+    modelXPos = 720;
+    modelYPos = 395;
+    
+    //set gl settings
+    //glEnable(GL_DEPTH_TEST);
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_COLOR_MATERIAL);
+    
+    //init lighting
+    glLightfv (GL_LIGHT0, GL_POSITION, lightOnePosition);
+    glLightfv (GL_LIGHT0, GL_DIFFUSE, lightOneColor);
+    glEnable (GL_LIGHT0);
+    glLightfv (GL_LIGHT1, GL_POSITION, lightTwoPosition);
+    glLightfv (GL_LIGHT1, GL_DIFFUSE, lightTwoColor);
+    glEnable (GL_LIGHT1);
+    
+    //load model
+    string boneFileName;
+    switch (trauma) {
+        case MNH_FAL_ANTEMORTEM:
+            boneFileName = "Skull_321498_aka_843-reduced10k-tex.3ds";
+            break;
+        case MNH_FAL_PERIMORTEM:
+            boneFileName = "Cranium_209434-reduced10k.3ds";
+            break;
+        default:
+            break;
+    }
+    
+    boneModel.loadModel(boneFileName, 3);
+    model3DS* model = (model3DS*)boneModel.model;
+    
+    //boneModel info 
+    for (int i=0; i<model->m_meshes.size(); i++) {
+        mesh3DS* m3ds = &model->m_meshes[i];
+        cout << endl;
+        cout << "# of model verts "  << m3ds->m_vertices.size() << endl;
+        cout << "# of model normals " << m3ds->m_normals.size() << endl;
+        cout << "# of model faces " << m3ds->m_faces.size() << endl;
+    }
+    
+    //set model to right-side-up
+    boneModel.setRotation(0, 180.0, 0, 0, 1);
+    //set initial scale
+    boneModel.setScale(0.75, 0.75, 0.75);
+    //set initial pos
+    boneModel.setPosition(modelXPos, modelYPos, 0);
+}
 
+//--------------------------------------------------------------
+void researchScene::drawModel(){
+    glPushMatrix();
+        glEnable (GL_LIGHTING);
+        //draw in middle of the screen
+        glTranslatef(modelXPos,modelYPos,0);
+        //tumble according to mouse
+        glRotatef(-touchY,1,0,0);
+        glRotatef(touchX,0,1,0);
+        glTranslatef(-modelXPos,-modelYPos,0);
+        
+        ofSetColor(254, 254, 254, 255);
+        boneModel.draw();
+        glDisable(GL_LIGHTING);
+    glPopMatrix();
+}
 
 //--------------------------------------------------------------
 //Event Listeners
 
 //--------------------------------------------------------------
 void researchScene::touchDown(ofTouchEventArgs &touch){
-   
+     isTouchDown = true;
+    
     //active the buttons only for the first scene
     if(mgr.getCurScene() == 0) 
     {
@@ -182,6 +264,9 @@ void researchScene::touchMoved(ofTouchEventArgs &touch){
     hintB.touchMoved(touch); 
     back.touchMoved(touch);
     start.touchDown(touch);
+    
+    touchX = touch.x;
+    touchY = touch.y;
 
 
 }
@@ -189,6 +274,7 @@ void researchScene::touchMoved(ofTouchEventArgs &touch){
 
 //--------------------------------------------------------------
 void researchScene::touchUp(ofTouchEventArgs &touch){
+    isTouchDown = false;
     
     if (back.isPressed()) {
         mgr.setCurScene(MNH_RESEARCH_SCENE_FIRST);
@@ -213,15 +299,30 @@ void researchScene::touchUp(ofTouchEventArgs &touch){
     {
         if (anteB.isPressed())
         {
+            t = MNH_FAL_ANTEMORTEM;
             mgr.setCurScene(MNH_RESEARCH_SCENE_FIFTH);
             cout<<"anteB";
         }else if (periB.isPressed())
         {
+            t = MNH_FAL_PERIMORTEM;
             cout<<"periB";
         }else if(postB.isPressed())
         {
+            t = MNH_FAL_POSTMORTEM;
             cout<<"postB";
         }
     }
+}
 
+//--------------------------------------------------------------
+void researchScene::touchDoubleTap(ofTouchEventArgs &touch){
+    if(!firstDoubleTap) {
+        boneModel.setScale(1.0, 1.0, 1.0);
+    }
+    
+    if(firstDoubleTap) {
+        boneModel.setScale(0.75, 0.75, 0.75);
+    }
+    
+    firstDoubleTap = !firstDoubleTap;
 }
