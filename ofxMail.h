1a0d9d18ab77fbdb47155f34c77e6b3a588c023d
Miguel Bermudez <miguel@potiondesign.com>
2011-06-29 11:25:39 -0400

    added annotated models, cleaned up repo, added ignore file

---
+++ ofxMail.h
@@ -0,0 +1,16 @@
+//
+//  ofxMail.h
+//  mnhPrototype
+//
+//  Created by Miguel on 6/28/11.
+//  Copyright 2011 Potion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ofxMail : NSObject {
+    
+}
+
+@end
---
+++ ofxMail.mm
@@ -0,0 +1,14 @@
+//
+//  ofxMail.mm
+//  mnhPrototype
+//
+//  Created by Miguel on 6/28/11.
+//  Copyright 2011 Potion. All rights reserved.
+//
+
+#import "ofxMail.h"
+
+
+@implementation ofxMail
+
+@end
